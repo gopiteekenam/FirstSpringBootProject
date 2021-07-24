e8a0b099daafc3f367ff0f302469f53500d4f21b
sravya <sravya.teekenam@outlook.com>
2021-07-24 15:17:33 +0530

    my second java file

--- src/main/java/MySecondJavaClass.java
+++ src/main/java/MySecondJavaClass.java
@@ -1,5 +1,9 @@
 public class MyFirstJavaClass {
     public static void main(String args[]){
         System.out.println("this is my first java program");
+		if(1 ==2 )
+		{
+			System.out.println(" 1 is equal to 2");
+		}
     }
 }
